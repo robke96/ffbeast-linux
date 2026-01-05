8148181092c9e44e551d62744cda1a4545ac831f
robke96 <robertaszencius@gmail.com>
2026-01-05 11:11:20 +0200

    temp: force dark theme

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/theme"
 	"github.com/robke96/ffbeast-linux/internal/device"
 	"github.com/robke96/ffbeast-linux/internal/ui"
 )
 
+type forcedVariant struct {
+	fyne.Theme
+	variant fyne.ThemeVariant
+}
+
 func main() {
 	a := app.New()
+	// force dark theme for now, in future todo: rely on system default?
+	a.Settings().SetTheme(&forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantDark})
+
 	w := a.NewWindow("FFBeastLinux")
 
 	dev := device.NewDevice()
